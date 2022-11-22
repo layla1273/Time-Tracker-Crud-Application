48572ea98058563b3f2715e2dd41bc223a4c120e
layla1273 <84575233+layla1273@users.noreply.github.com>
2022-11-22 17:18:53 +0100

    Create nav.php

---
+++ views/nav.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+        <li><a href="/">theTrackerApp</a></li>
+        <li><a href="/controllers/project_list.php">Projects list</a></li>
+        <li><a href="/controllers/task_list.php">Tasks list</a></li>
+        <li><a href="/controllers/project.php">Add project</a></li>
+        <li><a href="/controllers/task.php">Add task</a></li>
+        <li><a href="/controllers/reports.php">Reports</a></li>
+    </ul>
+</nav>
